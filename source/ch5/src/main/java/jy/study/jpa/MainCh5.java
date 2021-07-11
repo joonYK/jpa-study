f158fb62ddd607f50ef6f7a59da71abdef89f162
JY <kims2606@gmail.com>
2021-07-11 22:19:08 +0900

    연관관계 매핑 기초 (단방향 관계) 작성중

---
+++ source/ch5/src/main/java/jy/study/jpa/MainCh5.java
@@ -0,0 +1,15 @@
+package jy.study.jpa;
+
+public class MainCh5 {
+
+    public static void main(String[] args) {
+        Member member1 = new Member("member1", "회원1");
+        Member member2 = new Member("member2", "회원2");
+        Team team1 = new Team("team1", "팀1");
+
+        member1.setTeam(team1);
+        member2.setTeam(team1);
+
+        Team findTeam = member1.getTeam();
+    }
+}
---
+++ source/ch5/src/main/java/jy/study/jpa/Member.java
@@ -0,0 +1,22 @@
+package jy.study.jpa;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Member {
+
+    private String id;
+
+    private String username;
+
+    private Team team;
+
+    public Member(String id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+}
