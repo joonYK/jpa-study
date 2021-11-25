8991ddec8ad42c0a7d75ce28a65f13f883946845
JY <kims2606@gmail.com>
2021-11-25 23:14:47 +0900

    QueryDSL study - JPA, DB 설정

--- source/querydsl/src/test/java/jy/study/jpa/querydsl/QuerydslApplicationTests.java
+++ source/querydsl/src/test/java/jy/study/jpa/querydsl/QuerydslApplicationTests.java
@@ -3,16 +3,19 @@ package jy.study.jpa.querydsl;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jy.study.jpa.querydsl.entity.Hello;
 import jy.study.jpa.querydsl.entity.QHello;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
 	@Autowired
@@ -24,13 +27,14 @@ class QuerydslApplicationTests {
 		em.persist(hello);
 
 		JPAQueryFactory queryFactory = new JPAQueryFactory(em);
-		QHello qHello = new QHello("h");
+		QHello qHello = QHello.hello;
 
 		Hello result = queryFactory
 				.selectFrom(qHello)
 				.fetchOne();
 
-		Assertions.assertThat(result).isEqualTo(hello);
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 
 	}
 
