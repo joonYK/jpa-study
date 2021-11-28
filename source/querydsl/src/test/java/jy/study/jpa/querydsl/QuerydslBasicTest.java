12b5723b04be56dcb2155978fd1ab850618e6739
JY <kims2606@gmail.com>
2021-11-28 22:42:32 +0900

    QueryDSL study - 정렬

--- source/querydsl/src/test/java/jy/study/jpa/querydsl/QuerydslBasicTest.java
+++ source/querydsl/src/test/java/jy/study/jpa/querydsl/QuerydslBasicTest.java
@@ -127,4 +127,29 @@ public class QuerydslBasicTest {
                 .selectFrom(member)
                 .fetchCount();
     }
+
+    /**
+     * 1. 회원 나이 내림차순
+     * 2. 회원 이름 올림차순
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> members = queryFactory
+                .selectFrom(member)
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = members.get(0);
+        Member member6 = members.get(1);
+        Member memberNull = members.get(2);
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+
+    }
 }
