70c5cce4938ec84412928a106c14698bd61bdd6b
JY <kims2606@gmail.com>
2021-11-29 20:33:08 +0900

    QueryDSL study - 조인

--- source/querydsl/src/test/java/jy/study/jpa/querydsl/QuerydslBasicTest.java
+++ source/querydsl/src/test/java/jy/study/jpa/querydsl/QuerydslBasicTest.java
@@ -221,4 +221,40 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
+
+    @Test
+    public void join() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                //.leftJoin(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /**
+     * 세타 조인
+     * 회원의 이름이 팀 이름과 같은 회원 조회 (연관관계 없음)
+     */
+    @Test
+    public void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+
+        List<Member> members = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(members)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
 }
