ae58db1347037bc9c3416c9a5a3200476c334d0b
JY <kims2606@gmail.com>
2021-11-29 22:47:44 +0900

    QueryDSL study - 상수, 문자 더하기

--- source/querydsl/src/test/java/jy/study/jpa/querydsl/QuerydslBasicTest.java
+++ source/querydsl/src/test/java/jy/study/jpa/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package jy.study.jpa.querydsl;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jy.study.jpa.querydsl.entity.Member;
 import jy.study.jpa.querydsl.entity.QMember;
@@ -428,6 +429,33 @@ public class QuerydslBasicTest {
                         .otherwise("기타")
                 )
                 .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void constant() {
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void concat() {
+
+        //{username}_{age}
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetch();
 
         for (String s : result) {
