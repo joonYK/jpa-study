2b5382660d3080505895c25dda1eb2789aac4a00
JY <kims2606@gmail.com>
2021-11-30 10:38:43 +0900

    QueryDSL study - 프로젝션 결과 반환 (단일, 튜플)

---
+++ source/querydsl/src/test/java/jy/study/jpa/querydsl/BaseQuerydslTest.java
@@ -0,0 +1,40 @@
+package jy.study.jpa.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jy.study.jpa.querydsl.entity.Member;
+import jy.study.jpa.querydsl.entity.Team;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+public class BaseQuerydslTest {
+
+    @Autowired
+    protected EntityManager em;
+
+    protected JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+}
--- source/querydsl/src/test/java/jy/study/jpa/querydsl/QuerydslBasicTest.java
+++ source/querydsl/src/test/java/jy/study/jpa/querydsl/QuerydslBasicTest.java
@@ -4,17 +4,10 @@ import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import jy.study.jpa.querydsl.entity.Member;
 import jy.study.jpa.querydsl.entity.QMember;
-import jy.study.jpa.querydsl.entity.Team;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 import java.util.List;
@@ -24,33 +17,7 @@ import static jy.study.jpa.querydsl.entity.QMember.member;
 import static jy.study.jpa.querydsl.entity.QTeam.team;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@Transactional
-public class QuerydslBasicTest {
-
-    @Autowired
-    EntityManager em;
-
-    JPAQueryFactory queryFactory;
-
-    @BeforeEach
-    public void before() {
-        queryFactory = new JPAQueryFactory(em);
-        Team teamA = new Team("teamA");
-        Team teamB = new Team("teamB");
-        em.persist(teamA);
-        em.persist(teamB);
-
-        Member member1 = new Member("member1", 10, teamA);
-        Member member2 = new Member("member2", 20, teamA);
-
-        Member member3 = new Member("member3", 30, teamB);
-        Member member4 = new Member("member4", 40, teamB);
-        em.persist(member1);
-        em.persist(member2);
-        em.persist(member3);
-        em.persist(member4);
-    }
+public class QuerydslBasicTest extends BaseQuerydslTest {
 
     @Test
     public void startJPQL() {
