44a9a185bf92d870f679b5fa09d86512f68113bd
JY <kims2606@gmail.com>
2021-12-12 16:15:06 +0900

    QueryDSL study - 순수 JPA 동적 쿼리 사용한 API

---
+++ source/querydsl/src/main/java/jy/study/jpa/querydsl/controller/MemberController.java
@@ -0,0 +1,22 @@
+package jy.study.jpa.querydsl.controller;
+
+import jy.study.jpa.querydsl.dto.MemberSearchCondition;
+import jy.study.jpa.querydsl.dto.MemberTeamDto;
+import jy.study.jpa.querydsl.repository.MemberJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
